3ed2870e87d2f05851b8a2e2d6ed2b271e55255e
henri <henri1i.dev@gmail.com>
2023-01-06 01:11:17 -0300

    feat: create user repository implementation

---
+++ src/user/domain/repositories.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use async_trait::async_trait;
+
+use crate::infra::uuid::Uuid;
+use crate::user::domain::entities::User;
+
+#[async_trait]
+pub trait Repository {
+    async fn create(&self, user: User) -> Result<()>;
+    async fn find(&self, uuid: Uuid) -> Result<User>;
+    async fn list(&self) -> Result<Vec<User>>;
+    async fn delete(&self, user: Uuid) -> Result<()>;
+}
