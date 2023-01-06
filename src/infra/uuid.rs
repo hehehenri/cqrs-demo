5b1b6f7ba56f7a10e5e02083598338b930800cd1
henri <henri1i.dev@gmail.com>
2023-01-05 22:51:27 -0300

    feat: create uuid struct with try from impl

---
+++ src/infra/uuid.rs
@@ -0,0 +1,33 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Uuid {
+    value: uuid::Uuid
+}
+
+impl TryFrom<String> for Uuid {
+    type Error = &'static str;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let result = uuid::Uuid::from_str(value.as_str());
+
+        match result {
+            Ok(uuid) => Ok(Self { value: uuid }),
+            Err(error) => Err("Failed to parse string into Uuid")
+        }
+    }
+}
+
+impl Uuid {
+    pub fn new() -> Self {
+        Self {
+            value: uuid::Uuid::default()
+        }
+    }
+}
+
+impl ToString for Uuid {
+    fn to_string(&self) -> String {
+        self.value.to_string()
+    }
+}
